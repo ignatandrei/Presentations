a63047b238e8e92e9960ca5b70d8abdd7a1cc5ce
ignatandrei <ignatandrei@yahoo.com>
2019-06-09 11:34:33 +0300

    adding demo

--- 2019/shorts/NetCoreGlobalTools/runNetCore/runNetCoreWin.bat
+++ 2019/shorts/NetCoreGlobalTools/runNetCore/runNetCoreWin.bat
@@ -7,6 +7,19 @@ pause
 echo dotnet property
 docker exec -it netcore_tools_image dotnet-property NetCoreTestProject/NetCoreTestProject.csproj AssemblyVersion:"3.0.0.0"
 pause
+
+echo test - step 1 dotnet test
+docker exec -it netcore_tools_image  dotnet test NetCoreTestProject/NetCoreTestProject.csproj --logger trx  --logger "console;verbosity=normal" --collect "Code coverage"
+pause
+echo test - step 2 coverlet
+docker exec -it netcore_tools_image  coverlet NetCoreTestProject/bin/Debug/netcoreapp2.1/NetCoreTestProject.dll --target "dotnet" --targetargs "test NetCoreTestProject/NetCoreTestProject.csproj --configuration Debug --no-build"  --format opencover --exclude "[xunit*]*" --exclude "[*]NetCoreTestProject*"
+pause
+echo test - step 3 report generator
+docker exec -it netcore_tools_image  reportgenerator "-reports:coverage.opencover.xml" "-targetdir:coveragereport" "-reporttypes:HTMLInline;HTMLSummary;Badges"
+pause
+echo test - step 4 copy results
+docker cp netcore_tools_image:/usr/app/coveragereport .
+pause
 echo watch
 echo http://localhost:5000/api/values
-docker exec -it  netcore_tools_image  dotnet watch -p newNetCoreWebApi/newNetCoreWebApi.csproj run --urls=http://+:5000
+docker exec -it  netcore_tools_image  dotnet watch -p NetCoreSimple/NetCoreSimple.csproj run --urls=http://+:5000
