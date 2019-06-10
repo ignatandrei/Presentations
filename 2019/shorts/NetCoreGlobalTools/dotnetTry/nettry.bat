2fdc46e8b5a84919bf93f805432525253638daea
Andrei Ignat <ignatandrei@yahoo.com>
2019-06-10 07:14:56 +0300

    start aplication

--- 2019/shorts/NetCoreGlobalTools/dotnetTry/nettry.bat
+++ 2019/shorts/NetCoreGlobalTools/dotnetTry/nettry.bat
@@ -3,4 +3,4 @@ docker build --tag netcore_try_container --file nettry.txt .
 pause
 docker run --rm -d -p 5000:5000 --name netcore_try_image netcore_try_container
 pause
-http://localhost:5000/QuickStart.md
+START http://localhost:5000/QuickStart.md
