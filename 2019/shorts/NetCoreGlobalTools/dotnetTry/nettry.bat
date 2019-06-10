19500a26a53a036430aafaa3012718b8cd82e98c
Andrei Ignat <ignatandrei@yahoo.com>
2019-06-10 07:06:07 +0300

    first .net try

---
+++ 2019/shorts/NetCoreGlobalTools/dotnetTry/nettry.bat
@@ -0,0 +1,6 @@
+cls
+docker build --tag netcore_try_container --file nettry.txt .
+pause
+docker run --rm -d -p 5000:5000 --name netcore_try_image netcore_try_container
+pause
+http://localhost:5000/QuickStart.md
