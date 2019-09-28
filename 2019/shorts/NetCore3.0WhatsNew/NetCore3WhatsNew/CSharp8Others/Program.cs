648f4f0c8761efe74da116af6490843c8d9cfcd3
Andrei Ignat <ignatandrei@yahoo.com>
2019-09-28 15:31:37 +0300

    examples

--- 2019/shorts/NetCore3.0WhatsNew/NetCore3WhatsNew/CSharp8Others/Program.cs
+++ 2019/shorts/NetCore3.0WhatsNew/NetCore3WhatsNew/CSharp8Others/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharp8Others
 {
@@ -14,6 +15,59 @@ namespace CSharp8Others
             StaticLocalFunctions();
             Nullable();
             Version();
+            Indexes();
+            NullCoalescing();
+        }
+
+        private static void NullCoalescing()
+        {
+            List<int> list=new List<int>();
+            AddToList(list, 1);
+            AddToList(list, 2);
+        }
+        static void AddToList(List<int> a, int b)
+        {
+            a ??= new List<int>();
+            //other operations
+
+            a.Add(b);
+
+        }
+
+        private static void Indexes()
+        {
+            var words = new string[]
+{
+                // index from start    index from end
+    "The",      // 0                   ^9
+    "quick",    // 1                   ^8
+    "brown",    // 2                   ^7
+    "fox",      // 3                   ^6
+    "jumped",   // 4                   ^5
+    "over",     // 5                   ^4
+    "the",      // 6                   ^3
+    "lazy",     // 7                   ^2
+    "dog"       // 8                   ^1
+};
+            Console.WriteLine($"The last word is {words[^1]}");
+
+            var xAll = words[0..^0];
+
+            var xStart = words[2..];
+            var xStart1 = words[^2..];
+            var xEnd = words[..2];            
+            var xEnd1 = words[^2..];
+
+            var r = 1..4;
+            
+            var ex = words[r];
+
+            Console.WriteLine($"{words.Length} {xAll.Length}");
+            
+            
+
+
+
         }
 
         private static void Version()
@@ -47,13 +101,13 @@ namespace CSharp8Others
             {
                 //y = 10;
                 return a + b;
-                
+
             }
         }
 
         private static void UsingUsage()
         {
-            using(var x=new MyUsing())
+            using (var x = new MyUsing())
             {
                 x.WriteText();
             }
