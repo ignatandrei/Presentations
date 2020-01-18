2df6f36612d8b301555f29423ad766171443404f
ignatandrei <ignatandrei@yahoo.com>
2020-01-18 16:43:46 +0200

    consoles for usual plugins demo

---
+++ 2020/NETCore3Plugins/MethodsLoadingPlugins/ConsoleLoadPlugin/Program.cs
@@ -0,0 +1,30 @@
+﻿using MethodsLoadingPlugins;
+using System;
+
+namespace ConsoleLoadPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate[] plugins = LoadPlugins();
+            var o = new Order(plugins);
+            o.lines.Add(new LineOrder()
+            {
+                Number = 10,
+                sellGood = new SellGood()
+                {
+                    Name = "bread",
+                    Price = 15
+                }
+            });
+
+            Console.WriteLine(o.CalculateAmount());
+        }
+
+        private static Calculate[] LoadPlugins()
+        {
+            return null;
+        }
+    }
+}
