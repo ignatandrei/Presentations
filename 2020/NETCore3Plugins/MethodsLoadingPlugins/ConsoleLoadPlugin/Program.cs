0d349abc41196c202bf0157e33d4398bd5ac44ba
ignatandrei <ignatandrei@yahoo.com>
2020-01-18 17:32:19 +0200

    load plugins old school

--- 2020/NETCore3Plugins/MethodsLoadingPlugins/ConsoleLoadPlugin/Program.cs
+++ 2020/NETCore3Plugins/MethodsLoadingPlugins/ConsoleLoadPlugin/Program.cs
@@ -1,13 +1,50 @@
 ﻿using MethodsLoadingPlugins;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace ConsoleLoadPlugin
 {
+    class PluginLoadContext : AssemblyLoadContext
+    {
+        private AssemblyDependencyResolver _resolver;
+
+        public PluginLoadContext(string pluginPath)
+        {
+            _resolver = new AssemblyDependencyResolver(pluginPath);
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            string libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+            if (libraryPath != null)
+            {
+                return LoadUnmanagedDllFromPath(libraryPath);
+            }
+
+            return IntPtr.Zero;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Calculate[] plugins = LoadPlugins();
+            Console.WriteLine($" number of plugins : {plugins.Length}");
             var o = new Order(plugins);
             o.lines.Add(new LineOrder()
             {
@@ -24,7 +61,38 @@ namespace ConsoleLoadPlugin
 
         private static Calculate[] LoadPlugins()
         {
-            return null;
+            var list = new List<Calculate>();
+            var f = Directory.GetFiles("plugins", "*.dll");
+            foreach(var item in f)
+            {
+                Assembly a = Assembly.LoadFrom(item);
+                var types = a.GetTypes();
+                //var p = new PluginLoadContext(item);
+                //var ass= p.LoadFromAssemblyName()
+                //var types = p.Assemblies.SelectMany(it => it.GetTypes()).ToArray();
+                var s = types.Count();
+                //var calcs = types.Where(it => it.IsAssignableFrom(typeof(Calculate))). Cast<Calculate>().ToArray();
+                var calcs = types.Where(it =>
+                {
+                    try
+                    {
+                        var s = a.CreateInstance(it.FullName);
+                        bool IsCalculate= s is Calculate c;
+                        return IsCalculate;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                })
+                    .Select(it=> (Calculate)a.CreateInstance(it.FullName))
+                    .ToArray();
+
+                if (calcs.Length > 0)
+                    list.AddRange(calcs);
+
+            }
+            return list.ToArray();
         }
     }
 }
