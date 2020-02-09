d632e929ef5a98acca52ae3aead911c6f10b4481
ignatandrei <ignatandrei@yahoo.com>
2020-02-09 17:12:00 +0200

    running .net core

---
+++ 2020/DockerForDevs/RunApp/runNetCore/newNetCore21/NetCoreSimpleBusinessLogic/MyImportantClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace NetCoreSimpleBusinessLogic
+{
+    public class MyImportantClass
+    {
+        public async Task<int> Divide(int x, int y)
+        {
+			//change this to 0.5 or 1
+            await Task.Delay(1); 
+            //security
+            if (y == 0)
+                throw new ArgumentException("cannot divide by 0");
+            return x / y;
+        }
+
+
+    }
+}
--- 2020/DockerForDevs/RunApp/runNetCore/runNetCoreWin.bat
+++ 2020/DockerForDevs/RunApp/runNetCore/runNetCoreWin.bat
@@ -1,6 +1,6 @@
 cls
 ECHO please be aware of absolute path here %cd%
 docker build --tag netcore_modify_image --file runNetCore.txt .
-docker run --rm -d -p 5000:5000 --mount type=bind,source=%cd%\newNetCore,target=/usr/app --name netcore_modify_container netcore_modify_image 
-echo http://localhost:5000/api/values
-docker exec -it  netcore_modify_container  dotnet watch -p newNetCoreWebApi/newNetCoreWebApi.csproj run --urls=http://+:5000
+docker run --rm -d -p 5000:5000 --mount type=bind,source=%cd%\newNetCore21,target=/usr/app --name netcore_modify_container netcore_modify_image 
+echo http://localhost:5000/api/simplediv/ping
+docker exec -it  netcore_modify_container  dotnet watch -p NetCoreSimple/NetCoreSimple.csproj run --urls=http://+:5000
