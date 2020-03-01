03a3345341c17d118efdb60c4527c17b093b99ba
Andrei Ignat <ignatandrei@yahoo.com>
2020-03-01 23:55:16 +0200

    find not ref, finding expression has changed

--- 2020/Ang8vsAng9/ang8/newAng8/src/app/app.component.ts
+++ 2020/Ang8vsAng9/ang8/newAng8/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   title = 'newAng8';
+  theValue =1;
+  openProgressbar = new Subject<boolean>();
+  public changeSomeValue():boolean{
+    this.openProgressbar.next(true);
+    return (this.theValue % 2) === 1;
+  }
+
+  ngOnInit() {
+    this.openProgressbar
+      .asObservable()
+      .subscribe(ifOpen => {
+        this.theValue +=1;
+    });
+  }
+
 }
--- 2020/Ang8vsAng9/ang9/newAng9/src/app/app.component.ts
+++ 2020/Ang8vsAng9/ang9/newAng9/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+  
+  
   title = 'newAng9';
+  theValue =1;
+  openProgressbar = new Subject<boolean>();
+  public changeSomeValue():boolean{
+    this.openProgressbar.next(true);
+    return (this.theValue % 2) === 0;
+  }
+
+  ngOnInit() {
+    this.openProgressbar
+      .asObservable()
+      .subscribe(ifOpen => {
+        this.theValue +=1;
+    });
+  }
 }
