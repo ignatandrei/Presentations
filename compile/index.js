45dc9796547d7342bdaeb4d65a3a74e668097375
ignatandrei <ignatandrei@yahoo.com>
2020-06-06 16:18:46 +0300

    more files

--- compile/index.js
+++ compile/index.js
@@ -21,7 +21,10 @@ archive.createZip();
 
 
 file = 'AngNetCoreDemo';
+fileHtml='readme.txt';
+
 archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
  
 // Create the zip file.
 archive.createZip();
