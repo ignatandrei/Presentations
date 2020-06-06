c72799f4f0678935eb949c89276ab3a0d929fc0c
ignatandrei <ignatandrei@yahoo.com>
2020-06-06 16:49:49 +0300

    all prezentations

--- compile/index.js
+++ compile/index.js
@@ -31,39 +31,53 @@ archive.createZip();
 
 
 file = 'AzureDevOps';
+fileHtml='azureDevOps.html';  
 archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
- 
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+
 // Create the zip file.
 archive.createZip();
 
 file = 'NetCore3.0WhatsNew';
+fileHtml='presentation.html';  
+
 archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
- 
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+
 // Create the zip file.
 archive.createZip();
 
 file = 'NetCoreGlobalTools';
+fileHtml='index.html';  
 archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
- 
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+
 // Create the zip file.
 archive.createZip();
 
 
 folder='../2020/';
 file='Ang8vsAng9'
+fileHtml='presentation/Ang8vsAng9.html';  
 archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
  
 // Create the zip file.
 archive.createZip();
 
 file='DockerForDevs'
 archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
- 
+fileHtml='presentation/Docker.html';  
+
 // Create the zip file.
 archive.createZip();
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 
 file='NETCore3Plugins'
+fileHtml='presentation.html';  
+
 archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
  
 // Create the zip file.
 archive.createZip();
