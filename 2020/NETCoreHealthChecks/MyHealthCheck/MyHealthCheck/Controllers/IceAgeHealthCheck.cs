397133183315f8b1c54ea676e195b56292e4657c
ignatandrei <ignatandrei@yahoo.com>
2020-07-05 11:54:31 +0300

    hc ice age

---
+++ 2020/NETCoreHealthChecks/MyHealthCheck/MyHealthCheck/Controllers/IceAgeHealthCheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyHealthCheck.Controllers
+{
+    public class IceAgeHealthCheck : Controller
+    {
+        public string Index()
+        {
+            return "ping " + DateTime.UtcNow.ToString("o");
+        }
+    }
+}
