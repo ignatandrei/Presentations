fd89900603bb4a2fa8ae20c25dbc4f1ae30236ea
ignatandrei <ignatandrei@yahoo.com>
2020-08-08 19:24:11 +0300

    tracing

--- compile/index.js
+++ compile/index.js
@@ -112,6 +112,16 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 archive.createZip();
 
 
+
+file='ActvityTracing'
+fileHtml='presentation/ActvityTracing.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
+
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
     name: 'index.html',
