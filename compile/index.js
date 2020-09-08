2fc52e769a599a8534f7133d8af5ea3b8b88d1e0
Andrei Ignat <ignatandrei@yahoo.com>
2020-09-08 06:33:49 +0300

    deploy to prez

--- compile/index.js
+++ compile/index.js
@@ -121,6 +121,14 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+file='TestingWebAPI'
+fileHtml='presentation/TestingWebAPI.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
 
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
