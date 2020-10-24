17922a3d22874df3fa8fafabd0f3522688c56a28
ignatandrei <ignatandrei@yahoo.com>
2020-10-24 08:54:14 +0300

    start webapi

---
+++ 2020/WebAPIBP/code/Visibility/Visibility/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Visibility.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        //Answer to the right - delete spaces                                                                                                                TODO: put [HttpGet]
+        public string Get()
+        {
+            return "http://msprogrammer.serviciipeweb.ro/2020/10/05/asp-net-core-webapi-should-must-have/";
+        }
+    }
+}
