ac371cda0628ec1d01e81c1b8b2d379ea1c14d7b
ignatandrei <ignatandrei@yahoo.com>
2020-11-10 10:53:05 +0200

    added webapi

--- compile/index.js
+++ compile/index.js
@@ -129,6 +129,13 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+file='WebAPIBP'
+fileHtml='presentation/WebAPIBP.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
 
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
