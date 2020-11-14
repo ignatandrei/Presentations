e44acb957aa9570665ec386394f49d27b2d9d011
Andrei Ignat <ignatandrei@yahoo.com>
2020-11-14 10:15:20 +0200

    Demo

---
+++ 2020/NET5/code/DemoNet5/DemoClickOnce/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DemoClickOnce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoClickOnce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //here it works , in razor not
+            var p = ThisAssembly.Project.AssemblyName;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 2020/NET5/code/DemoNet5/DemoNetCoreClickOnceConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoNetCoreClickOnceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
