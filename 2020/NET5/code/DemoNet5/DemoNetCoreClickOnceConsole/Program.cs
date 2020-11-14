419413ec4035a2fe613a5dbf424d3a0d37be0815
Andrei Ignat <ignatandrei@yahoo.com>
2020-11-14 11:01:01 +0200

    net 5

--- 2020/NET5/code/DemoNet5/DemoNetCoreClickOnceConsole/Program.cs
+++ 2020/NET5/code/DemoNet5/DemoNetCoreClickOnceConsole/Program.cs
@@ -6,6 +6,9 @@ namespace DemoNetCoreClickOnceConsole
     {
         static void Main(string[] args)
         {
+//            2020.111.114.1018
+
+//https://github.com/ignatandrei/presentations/clickonce/democonsole
             Console.WriteLine($"Hello World from {ThisAssembly.Project.AssemblyName} version {ThisAssembly.Info.Version} ");
         }
     }
