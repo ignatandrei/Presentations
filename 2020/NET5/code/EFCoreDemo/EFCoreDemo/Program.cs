e892d9d3e4cec306caecdc877af871f289946251
ignatandrei <ignatandrei@yahoo.com>
2020-12-08 13:25:37 +0200

    split

--- 2020/NET5/code/EFCoreDemo/EFCoreDemo/Program.cs
+++ 2020/NET5/code/EFCoreDemo/EFCoreDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 
 namespace EFCoreDemo
@@ -10,8 +11,14 @@ namespace EFCoreDemo
             var cont = new testsContext();
             //cont.Database.EnsureCreated();
             Console.WriteLine("Nr deps:"+ cont.Departments.Count());
+            //dictionary in EF
             var obj = (cont.DepartmentWithNumber.First());
             Console.WriteLine(obj["IDDepartment"]);
+
+            //split query
+            var depAll = cont.Departments.Include(it => it.Employees).ToArray();
+            var depSplit=cont.Departments.AsSplitQuery().Include(it => it.Employees).ToArray();
+
         }
     }
 }
