dd59a3556d19c82b4bbc3ffe91d25ac35534760a
ignatandrei <ignatandrei@yahoo.com>
2020-12-08 13:09:56 +0200

    efore demo

---
+++ 2020/NET5/code/EFCoreDemo/EFCoreDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cont = new testsContext();
+            cont.Database.EnsureCreated();
+            Console.WriteLine("Nr deps:"+ cont.Departments.Count());
+            var obj = (cont.DepartmentWithNumber.First());
+            Console.WriteLine(obj["IDDepartment"]);
+        }
+    }
+}
