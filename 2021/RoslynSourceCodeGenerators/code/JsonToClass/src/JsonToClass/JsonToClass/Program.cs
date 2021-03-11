c10746a6057864bfef0ce3241ecaf58d22a69635
ignatandrei <ignatandrei@yahoo.com>
2021-03-11 06:49:38 +0200

    json

---
+++ 2021/RoslynSourceCodeGenerators/code/JsonToClass/src/JsonToClass/JsonToClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JsonToClass.Json.Persons;
+using Microsoft.Extensions.Configuration;
+namespace JsonToClass.Json.Persons
+{
+    partial class Person
+    {
+        public string FullName() => FirstName + LastName;
+    }
+}
+namespace JsonToClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person();
+            p1.Blog = "http://msprogrammer.serviciipeweb.ro/";
+            var config = new ConfigurationBuilder()
+              .AddJsonFile("persons.json")
+              .Build();
+
+            var p = config.Get<Person>();
+            Console.WriteLine($"{p.FirstName}");
+
+            var p2 = Person.FromConfig(config);
+            //Console.Write(p2.FirstName);
+        }
+    }
+}
