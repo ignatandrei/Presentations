9be0387521a3edafd81b8d0b6186e803a6165c6d
ignatandrei <ignatandrei@yahoo.com>
2021-03-11 09:09:44 +0200

    more examples

---
+++ 2021/RoslynSourceCodeGenerators/code/SourceGenerators/GeneratedDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GeneratedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+
+            Console.WriteLine("\n\nRunning CsvGenerator:\n");
+            UseCsvGenerator.Run();
+
+            Console.WriteLine("\n\nRunning XmlSettings:\n");
+            UseXmlSettingsGenerator.Run();
+
+        }
+    }
+}
