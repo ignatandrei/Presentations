be5aa6ab47f811a73378afdf393ee76f092b80c3
ignatandrei <ignatandrei@yahoo.com>
2021-03-11 17:42:53 +0200

    enum with key values

---
+++ 2021/RoslynSourceCodeGenerators/code/SourceGenerators/InteractingWithCode/AdvEx/AdvEx/Program.cs
@@ -0,0 +1,41 @@
+﻿using AOPMethodsCommon;
+using System;
+
+
+namespace AdvEx
+{
+    [AutoEnum(template = EnumMethod.CustomTemplateFile, CustomTemplateFileName ="enum.txt")]
+    public enum State
+    {
+        None = 0,
+        Active = 1,
+        Wait=2
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseEnum();
+        }
+        static void ParseEnum()
+        {
+            Console.WriteLine("Please give the state");
+            foreach (var item in enumState.KeyValues)
+            {
+                Console.Write($"{item.Value} => {item.Key} ");
+            }
+            Console.WriteLine();
+            var s = Console.ReadLine();
+
+            if(!Enum.TryParse<State>(s,true,out var res))
+            {
+                res = State.None;
+            }
+            
+            var res1 = enumState.ParseExactState(s,  State.None);
+
+            Console.WriteLine(res.ToString());
+            Console.WriteLine(res1.ToString());
+        }
+    }
+}
