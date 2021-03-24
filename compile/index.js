7ac4e5d6baafbb9eeb41eed0bd2107cb67bbafe8
ignatandrei <ignatandrei@yahoo.com>
2021-03-24 06:48:29 +0200

    Added RSGC

--- compile/index.js
+++ compile/index.js
@@ -146,6 +146,15 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+folder='../2021/';
+file='RoslynSourceCodeGenerators'
+fileHtml='presentation/RoslynSourceCodeGenerators.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
 
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
