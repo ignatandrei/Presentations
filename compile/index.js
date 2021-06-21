fe10b0e5e77046938dae0b3d2251c3eaf25a80ef
ignatandrei <ignatandrei@yahoo.com>
2021-06-21 15:31:09 +0300

    web api returns

--- compile/index.js
+++ compile/index.js
@@ -166,6 +166,15 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 archive.createZip();
 
 
+folder='../2021/';
+file='WebAPIReturns'
+fileHtml='presentation/WebAPIReturns.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
 
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
