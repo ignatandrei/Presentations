bde139bf20c242a942381089f4e0647081cdc7f1
ignatandrei <ignatandrei@yahoo.com>
2021-06-21 11:16:00 +0300

    first demo return webapi

---
+++ 2021/WebAPIReturns/code/BLReturnCode/BLReturnCode/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BLReturnCode
+{
+    public class Person : IValidatableObject
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(this.ID %2 ==0 )
+                yield return new ValidationResult("not valid ",new[] { nameof(ID) });
+        }
+    }
+
+}
---
+++ 2021/WebAPIReturns/code/BLReturnCode/DemoWebApp/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using BLReturnCode;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // TODO: try with 1 and 2
+        [HttpGet("{id}")]
+        public Person GetPerson(int id)
+        {
+            return RetrieveFromDatabase(id);
+        }
+
+        // try with 1 and 2
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetPerson404(int id)
+        {
+            var p = RetrieveFromDatabase(id);
+            if (p == null)
+                return NotFound($"{nameof(Person)} with {id} are not found");
+
+            return p;
+
+        }
+        //try with id 1 and 2
+        [HttpPost]
+        public int SavePerson(Person p)
+        {
+            // save to database, then
+            return p.ID;
+        }
+        private Person RetrieveFromDatabase(int id)
+        {
+            if (id % 2 == 0)
+                return new Person()
+                {
+                    ID = id,
+                    Name = "Andrei Ignat " + id
+                    
+                };
+
+
+            return null;
+        }
+    }
+}
