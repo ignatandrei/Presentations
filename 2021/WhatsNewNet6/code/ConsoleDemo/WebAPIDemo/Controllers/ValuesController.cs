09787d6cde3ebc1d9020d7cdc9dde3387fb16422
Andrei Ignat <ignatandrei@yahoo.com>
2021-11-23 22:27:56 +0200

    more demos

---
+++ 2021/WhatsNewNet6/code/ConsoleDemo/WebAPIDemo/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //Demo: AsyncEnumerable
+        // https://localhost:7115/api/Values/GetValues
+        [HttpGet]
+        public async IAsyncEnumerable<int> GetValues()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(2000);
+                yield return i;
+            }
+        }
+
+        //// GET api/<ValuesController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<ValuesController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<ValuesController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ValuesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
--- 2021/WhatsNewNet6/code/ConsoleDemo/WebAPIDemo/Controllers/WeatherForecastController.cs
+++ 2021/WhatsNewNet6/code/ConsoleDemo/WebAPIDemo/Controllers/WeatherForecastController.cs
@@ -1,36 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+//Demo: file scoped namespaces
+namespace WebAPIDemo.Controllers;
 
-namespace WebAPIDemo.Controllers
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            _logger = logger;
-        }
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var data= Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-            var c = new MyWeatherSourceGenerationContext();
+        return data;
+    }
 
-            return data;
-        }
+    [HttpPost]
+    public string? Data(WeatherForecast? wf)
+    {
+        wf = null;
+        VerifyNotNull(wf);
+        return wf?.ToString();
+    }
+    //Demo: CallerArgumentExpression
+    void VerifyNotNull<T>(T value,
+        [CallerMemberName] string? caller = null,
+        [CallerLineNumber] int? callerLineNumber = null,
+        [CallerFilePath] string? callerFilePath = null,
+[CallerArgumentExpression("value")] string? nameVar = null)
+    {
+        if (value == null)
+            throw new ArgumentException($"{nameVar} is null in function {caller} line {callerLineNumber} from file {callerFilePath}", nameVar);
+
     }
 }
