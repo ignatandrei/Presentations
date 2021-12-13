53b0646299c469c32f5e9b118eefe6e3e63834d4
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-13 12:38:54 +0200

    net 6

---
+++ 2021/WhatsNewNet6/code/EFCore6/EFCore6WebAPI/Department.cs
@@ -0,0 +1,19 @@
+﻿
+#nullable disable
+
+namespace EFCore6WebAPI;
+
+
+    public partial class Department
+    {
+        public Department()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public long Iddepartment { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+
---
+++ 2021/WhatsNewNet6/code/EFCore6/EFCore6WebAPI/globalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using System;
+global using System.Collections.Generic;
+global using EFCore6WebAPI;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
