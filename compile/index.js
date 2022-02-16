fa2cd9eea28cdb7b4dea90c52e791274e6ae3c0d
Andrei Ignat <ignatandrei@yahoo.com>
2022-02-16 07:59:11 +0200

    added blockly prezentation

--- compile/index.js
+++ compile/index.js
@@ -195,6 +195,15 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+folder='../2022/';
+file='Blockly'
+fileHtml='presentation/Blockly.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
 
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
