e8ed2b6179b23e068d68e9d2c161a361152f03fb
Andrei Ignat <ignatandrei@yahoo.com>
2022-11-02 21:36:51 +0200

    activity

--- 2022/gotech/presentation/code/OpenTelemetry/DemoOpenTelemetry/DemoOpenTelemetry/globals.cs
+++ 2022/gotech/presentation/code/OpenTelemetry/DemoOpenTelemetry/DemoOpenTelemetry/globals.cs
@@ -8,17 +8,33 @@ public class ActivityData
     static string serviceName = ThisAssembly.Project.AssemblyName;
     static ActivitySource MyActivitySource = new ActivitySource(serviceName);
 
-    public static IDisposable? AddActivity(
+    public static Activity? AddActivity(
         [CallerMemberName] string member = "",
          [CallerLineNumber] int line = 0,
          [CallerFilePath] string filePath = ""
         )
     {
-        var activity = MyActivitySource.StartActivity("SayHello");
 
+        var activity = MyActivitySource.StartActivity(member);
+        activity.SetStatus(ActivityStatusCode.Ok);
         activity?.SetTag("CallerMemberName", member);
         activity?.SetTag("line", line);
         activity?.SetTag("filePath", filePath);
+        return activity;
+    }
+    public static Activity? AddActivityException(
+        Exception ex,
+        [CallerMemberName] string member = "",
+         [CallerLineNumber] int line = 0,
+         [CallerFilePath] string filePath = ""
+        )
+    {
+        var activity = MyActivitySource.StartActivity(member);
+        activity.SetStatus(ActivityStatusCode.Error);
+        activity?.SetTag("CallerMemberName", member);
+        activity?.SetTag("line", line);
+        activity?.SetTag("filePath", filePath);
+        activity?.RecordException(ex);
         return activity;
     }
 }
