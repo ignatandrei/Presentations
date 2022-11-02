00ec120ef5d90d61531ed97f80cad254389ded66
Andrei Ignat <ignatandrei@yahoo.com>
2022-11-02 21:35:08 +0200

    make bl

---
+++ 2022/gotech/presentation/code/OpenTelemetry/DemoOpenTelemetry/DemoOpenTelemetry/Controllers/ForeCastData.cs
@@ -0,0 +1,36 @@
+﻿using AOPMethodsCommon;
+
+namespace DemoOpenTelemetry.Controllers
+{
+    [AutoMethods(CustomTemplateFileName = "../AutoMethod.txt", MethodPrefix = "auto", template = TemplateMethod.CustomTemplateFile)]
+    public partial class ForeCastData
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        [AOPMarkerMethod]
+        public async Task<WeatherForecast[]> GetData(int nr)
+        {
+            if (nr % 3 == 0)
+                throw new ArgumentException($"cannot have number {nr}");
+
+            var data = Enumerable.Range(1, nr)
+                .Select(async index => await FindForeCast(index))
+                .ToArray();
+            var ret = await Task.WhenAll(data);
+            return ret;
+        }
+        [AOPMarkerMethod]
+        public async Task<WeatherForecast> FindForeCast(int index)
+        {
+            await Task.Delay(Random.Shared.Next(100, 1000));
+            return new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            };
+        }
+    }
+}
--- 2022/gotech/presentation/code/OpenTelemetry/DemoOpenTelemetry/DemoOpenTelemetry/Controllers/WeatherForecastController.cs
+++ 2022/gotech/presentation/code/OpenTelemetry/DemoOpenTelemetry/DemoOpenTelemetry/Controllers/WeatherForecastController.cs
@@ -6,10 +6,7 @@ namespace DemoOpenTelemetry.Controllers
     [Route("[controller]/[action]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -19,18 +16,13 @@ namespace DemoOpenTelemetry.Controllers
         }
 
         [HttpGet("{nr}")]
-        public IEnumerable<WeatherForecast> GetData(int nr)
+        public async Task<WeatherForecast[]> GetData(int nr)
         {
-            if (nr % 3 == 0)
-                throw new ArgumentException($"cannot have number {nr}");
-
-            return Enumerable.Range(1, nr).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            //not using DI for clarity
+            ForeCastData forecast = new ();
+            return await forecast.GetData(nr);
         }
+        
+        
     }
 }
