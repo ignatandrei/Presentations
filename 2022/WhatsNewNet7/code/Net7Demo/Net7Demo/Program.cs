45119ca25ace6263ab8195ffdf31bccae92c510c
Andrei Ignat <ignatandrei@yahoo.com>
2022-12-11 10:28:21 +0200

    tar

---
+++ 2022/WhatsNewNet7/code/Net7Demo/Net7Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Formats.Tar;
+
+WriteLine("Net 7!");
+WriteLine("make tar");
+var dir = GetCurrentDirectory();
+if(dir.EndsWith(Path.DirectorySeparatorChar))
+    dir=dir.Substring(0, dir.Length - 1);
+
+string nameTar =dir + "andrei.tar" ;
+if (File.Exists(nameTar)) File.Delete(nameTar);
+WriteLine($"archive {dir} to {nameTar}");
+TarFile.CreateFromDirectory(dir,nameTar,true);
+
+
---
+++ 2022/WhatsNewNet7/code/Net7Demo/Net7Demo/globalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System;
+global using System.IO;
+
+global using static System.Console;
+global using static System.IO.Directory;
