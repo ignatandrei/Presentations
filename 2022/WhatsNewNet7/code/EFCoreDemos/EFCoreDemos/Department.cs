07d0977141142800999a72bf15ba03b66548a673
Andrei Ignat <ignatandrei@yahoo.com>
2022-12-12 20:55:02 +0200

    start efcore

---
+++ 2022/WhatsNewNet7/code/EFCoreDemos/EFCoreDemos/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreDemos;
+
+public partial class Department
+{
+    public long Iddepartment { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Employee> Employees { get; } = new List<Employee>();
+}
---
+++ 2022/WhatsNewNet7/code/EFCoreDemos/EFCoreDemos/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreDemos;
+
+public partial class Test
+{
+    public int? Id { get; set; }
+
+    public string? Name { get; set; }
+}
