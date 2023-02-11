3385ecd6ff3d80ed0fd69e47147ff6aac8c28783
Andrei Ignat <ignatandrei@yahoo.com>
2023-02-11 12:10:16 +0200

    first example
    This reverts commit 999e8ba4b939d165afbb53b719ee95741cc51882.

---
+++ 2023/react/code/react1/like_button.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class LikeButtonClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this in class';
+    }
+
+    return React.createElement(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like class '+ this.props.test
+    );
+  }
+}
+
+
+
+function LikeButtonFunc(props) {
+    const [liked, setLiked] = React.useState(false);
+  
+    if (liked) {
+      return 'You liked this in func!';
+    }
+  
+    return React.createElement(
+      'button',
+      {
+        onClick: () => setLiked(true),
+      },
+      'Like in func ' + props.test
+    );
+  }
+
+
+
+const domContainer = document.querySelector('#like_button_container_forClass');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement(LikeButtonClass , {test:"#message#"}));
+
+
+
+const domContainerFunc = document.querySelector('#like_button_container_forFunc');
+const rootFunc = ReactDOM.createRoot(domContainerFunc);
+rootFunc.render(React.createElement(LikeButtonFunc,{test:'!message!'}));
