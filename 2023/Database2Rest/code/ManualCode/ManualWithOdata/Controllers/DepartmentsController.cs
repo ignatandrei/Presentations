de42d466886eb6fa6fd524fb9e006142ad37d5d5
Andrei Ignat <ignatandrei@yahoo.com>
2023-06-12 23:04:37 +0300

    start odata

---
+++ 2023/Database2Rest/code/ManualCode/ManualWithOdata/Controllers/DepartmentsController.cs
@@ -0,0 +1,19 @@
+﻿using DalManual;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace ManualWithOdata.Controllers;
+
+public class DepartmentsController : ODataController
+{
+    private readonly TestsDatabase2RestContext context;
+
+    public DepartmentsController(TestsDatabase2RestContext context)
+    {
+        this.context = context;
+    }
+    public ActionResult<IQueryable<Department>> Get()
+    {
+        return Ok(context.Department);
+    }
+}
---
+++ 2023/Database2Rest/code/ManualCode/ManualWithOdata/Program.cs
@@ -0,0 +1,42 @@
+using DalManual;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.ModelBuilder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var modelBuilder = new ODataConventionModelBuilder();
+
+modelBuilder.EntitySet<Department>("Department").EntityType.HasKey(d=>d.Iddepartment);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddOData(
+    options => options.EnableQueryFeatures()
+    .AddRouteComponents(
+        routePrefix: "odata",
+        model: modelBuilder.GetEdmModel()));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TestsDatabase2RestContext>(options =>
+              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
