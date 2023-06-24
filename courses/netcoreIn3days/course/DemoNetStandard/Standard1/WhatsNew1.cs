5647b89ffdcc6d8a5f83a36743d184799ae52768
Andrei Ignat <ignatandrei@yahoo.com>
2023-06-24 09:52:37 +0300

    refactor

--- courses/netcoreIn3days/course/DemoNetStandard/Standard1/WhatsNew1.cs
+++ courses/netcoreIn3days/course/DemoNetStandard/Standard1/WhatsNew1.cs
@@ -6,6 +6,7 @@ namespace Standard1
     {
         public WhatsNew1()
         {
+            string m = Environment.MachineName;
             //string s = Environment.CurrentDirectory;
             //string userName = Environment.UserDomainName;
         }
