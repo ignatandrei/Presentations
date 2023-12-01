560943c71770cb4aaafdb3d543c7b91f67ad7c00
Andrei Ignat <ignatandrei@yahoo.com>
2023-12-01 09:47:01 +0200

    time zone provider

---
+++ 2023/Net8WhatsNew/code/Net8WhatsNew/Net8WhatsNew/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+Console.WriteLine("Zip file to stream");
+
+var curDir = Environment.CurrentDirectory;
+var pathToExtract = Path.Combine(curDir, "test");
+if(Directory.Exists(pathToExtract) )
+    Directory.Delete(pathToExtract, true);
+byte[] zipBytes;
+
+using (MemoryStream ms = new())
+{
+    ZipFile.CreateFromDirectory(curDir, ms);
+    zipBytes = ms.ToArray();
+}
+
+using MemoryStream ms1=new(zipBytes);
+ZipFile.ExtractToDirectory(ms1, pathToExtract);
+
+// time zone provider, use DI
+TimeProvider tp = TimeProvider.System;
+
+Console.WriteLine($"Data: {GetDayOfWeekNow(tp)} ");
+tp = new FakeTp();
+Console.WriteLine($"Data: {GetDayOfWeekNow(tp)} ");
+
+static DayOfWeek GetDayOfWeekNow(TimeProvider tp)
+{
+    return tp.GetUtcNow().DayOfWeek;
+}
---
+++ 2023/Net8WhatsNew/code/Net8WhatsNew/Net8WhatsNew/time.cs
@@ -0,0 +1,13 @@
+﻿class FakeTp : TimeProvider
+{
+
+
+
+
+    public override DateTimeOffset GetUtcNow()
+    {
+        return new DateTimeOffset(1970, 4, 16, 15, 01, 01, TimeSpan.Zero);
+    }
+
+
+}
