419efe2f317dc96483c5ced0b5630912c56d11fb
Andrei Ignat <ignatandrei@yahoo.com>
2023-12-02 20:11:57 +0200

    add prez

--- compile/index.js
+++ compile/index.js
@@ -310,6 +310,18 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+
+folder='../2023/';
+file='Net8WhatsNew'
+fileHtml='presentation/Net8WhatsNew.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
+
+
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
     name: 'index.html',
