f15eefe4c4e9eea3135ae613115a22bb1c389df5
Andrei Ignat <ignatandrei@yahoo.com>
2024-02-12 00:31:47 +0200

    added sql server

--- 2024/Aspire/code/ZipAsAService/ZipAsAService.AppHost/Program.cs
+++ 2024/Aspire/code/ZipAsAService/ZipAsAService.AppHost/Program.cs
@@ -9,13 +9,30 @@ var grafana =
                      .WithServiceBinding(containerPort: 3000, hostPort: 3000, name: "grafana-http", scheme: "http");
 
 
-var apiService = builder.AddProject<Projects.ZipAsAService_ApiService>("apiservice");
+var db = builder.AddSqlServer("ad");
+var addressBookDb = 
+    builder.AddSqlServerContainer("sqlserver")
+    // Mount the init scripts directory into the container.
+    .WithVolumeMount("./sqlserverconfig", "/usr/config", VolumeMountType.Bind)
+    // Mount the SQL scripts directory into the container so that the init scripts run.
+    .WithVolumeMount("../DatabaseContainers.ApiService/data/sqlserver", "/docker-entrypoint-initdb.d", VolumeMountType.Bind)
+    // Run the custom entrypoint script on startup.
+    .WithArgs("/usr/config/entrypoint.sh")
+    // Add the database to the application model so that it can be referenced by other resources.
+    .AddDatabase("AddressBook");
+
+
+var apiService = builder
+    .AddProject<Projects.ZipAsAService_ApiService>("apiservice")
+    .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"))
+    .WithReference(addressBookDb)
+;
+
 
 builder.AddProject<Projects.ZipAsAService_WebAssembly>("webfrontend")
     //.WithReference(cache)
     .WithReference(apiService)
-    .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"));
-;
+;    
 
 builder.AddProject("tests", "../ZipAsAService.BLLTests/ZipAsAService.BLLTests.csproj")
 
@@ -31,7 +48,7 @@ builder.AddNpmApp("angular", "../ZipAsAService.Angular")
     ;
 
 
-//TODO: add grafana
-//TODO: add sql server container or direct
+
+
 
 builder.Build().Run(); 
