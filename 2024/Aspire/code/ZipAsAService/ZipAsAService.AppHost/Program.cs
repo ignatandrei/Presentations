60e0c6df91aae146f84332d71cbd30f192ba5840
Andrei Ignat <ignatandrei@yahoo.com>
2024-02-11 09:43:43 +0200

    add grafan

--- 2024/Aspire/code/ZipAsAService/ZipAsAService.AppHost/Program.cs
+++ 2024/Aspire/code/ZipAsAService/ZipAsAService.AppHost/Program.cs
@@ -2,12 +2,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 //var cache = builder.AddRedis("cache");
 
+var grafana = 
+    builder.AddContainer("grafana", "grafana/grafana")
+                     .WithVolumeMount("../grafana/config", "/etc/grafana")
+                     .WithVolumeMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
+                     .WithServiceBinding(containerPort: 3000, hostPort: 3000, name: "grafana-http", scheme: "http");
+
+
 var apiService = builder.AddProject<Projects.ZipAsAService_ApiService>("apiservice");
 
 builder.AddProject<Projects.ZipAsAService_WebAssembly>("webfrontend")
     //.WithReference(cache)
     .WithReference(apiService)
-    ;
+    .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"));
+;
 
 builder.AddProject("tests", "../ZipAsAService.BLLTests/ZipAsAService.BLLTests.csproj")
 
@@ -18,10 +26,12 @@ builder.AddProject("winformsclient", "../ZipAsAService.WinForm/ZipAsAService.Win
 
 builder.AddNpmApp("angular", "../ZipAsAService.Angular")
     .WithReference(apiService)
-    .WithServiceBinding(containerPort: 4200, scheme: "http", env: "PORT")
+    .WithServiceBinding(containerPort: 5500, scheme: "http", env: "PORT")
     //.AsDockerfileInManifest()
     ;
+
+
 //TODO: add grafana
 //TODO: add sql server container or direct
-//
+
 builder.Build().Run(); 
