641f45692adcb12c8e4d9c892be52c4537a4d2d0
Andrei Ignat <ignatandrei@yahoo.com>
2024-09-02 07:04:28 -0700

    playwright

--- 2024/AspireAdvanced/code/AspireWithJavaScript/AspireJavaScriptTests/E2ETest.cs
+++ 2024/AspireAdvanced/code/AspireWithJavaScript/AspireJavaScriptTests/E2ETest.cs
@@ -1,11 +1,17 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+using System.Text.RegularExpressions;
+
 namespace AspireJavaScriptTests;
 
 [TestClass]
-public class E2ETest
+public class E2ETest: PageTest
 {
     [TestMethod]
     public async Task SimpleReadText()
     {
+        //await Task.Delay(1000);
+        //return;
         // Arrange
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.AspireJavaScript_AppHost>();
         appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
@@ -27,5 +33,30 @@ public class E2ETest
         Assert.IsTrue(content.Contains("app-root"));
         Assert.IsTrue(content.Contains("Weather"));
 
+    }
+    //pwsh bin/Debug/net8.0/playwright.ps1 install
+    //could be done in code , but this is not a playwright demo
+    [TestMethod]
+    public async Task RetrieveData()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.AspireJavaScript_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("angular");
+        var url = httpClient.BaseAddress?.ToString();
+        Assert.IsNotNull(url);
+        await Page.GotoAsync(url);
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Weather"));
+        await Page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
     }
 }
