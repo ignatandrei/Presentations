20ddae097880aae8ea81efe452f653cf63a28293
Andrei Ignat <ignatandrei@yahoo.com>
2024-09-01 20:34:04 -0700

    added ANGULAR example

---
+++ 2024/AspireAdvanced/code/AspireWithJavaScript/AspireJavaScript.AppHost/Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var weatherApi = builder.AddProject<Projects.AspireJavaScript_MinimalApi>("weatherapi")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("angular", "../AspireJavaScript.Angular")
+    .WithReference(weatherApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.Build().Run();
