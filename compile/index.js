3c50553eecd9362330cfc115b162841f375cdd24
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-08 18:16:32 +0300

    added packaga analyzer

--- compile/index.js
+++ compile/index.js
@@ -356,6 +356,15 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+folder='../2024/';
+file='PackageAnalyzer'
+fileHtml='presentation/PackageAnalyzer.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
     name: 'index.html',
