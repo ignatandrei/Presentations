2a5f96051109674f8a2c64d9f6848eeab81e56c8
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-12 21:52:02 +0300

    example  : api , screenshots ,videos

---
+++ 2024/Playwright/code/PlayrightDemo/Demo1_Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Playwright;
+
+Console.WriteLine("Demo Playwright!");
+
+using var playwright = await Playwright.CreateAsync();
+var request = await playwright.APIRequest.NewContextAsync();
+
+// Make a GET request to the weather forecast API
+var response = await request.GetAsync("http://localhost:5348/weatherforecast");
+
+// Check if the response is successful
+if (response.Ok)
+{
+    var responseBody = await response.TextAsync();
+    Console.WriteLine("Weather Forecast API Response:");
+    Console.WriteLine(responseBody);
+}
+else
+{
+    Console.WriteLine($"Failed to fetch weather forecast. Status: {response.Status}");
+}
---
+++ 2024/Playwright/code/PlayrightDemo/Demo2_Show/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Playwright;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+var curDir = Directory.GetCurrentDirectory();
+var curDirVideos = Path.Combine(curDir, "videos");
+if(!Directory.Exists(curDirVideos))
+{
+    Directory.CreateDirectory(curDirVideos);
+}
+var curDirScreenshots = Path.Combine(curDir, "screenshots");
+if (!Directory.Exists(curDirScreenshots))
+{
+    Directory.CreateDirectory(curDirScreenshots);
+}
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+var context = await browser.NewContextAsync(new()
+{
+    RecordVideoDir = curDirVideos
+});
+var page = await context.NewPageAsync();
+await page.Clock.InstallAsync(new()
+{
+    TimeDate = new DateTime(1970, 4, 16, 0, 0, 0)
+});
+await page.GotoAsync("http://localhost:5348/");
+var pathFilePng = Path.Combine(curDirScreenshots, "screenshot1.png");
+await page.ScreenshotAsync(new PageScreenshotOptions { Path = pathFilePng });
+Process.Start(new ProcessStartInfo(pathFilePng) { UseShellExecute = true });
+Console.WriteLine("Waiting for the page to load...");
+await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+var str = await page.ContentAsync();
+pathFilePng = Path.Combine(curDirScreenshots, "screenshot2.png");
+await page.ScreenshotAsync(new PageScreenshotOptions { Path = pathFilePng });
+Process.Start(new ProcessStartInfo(pathFilePng) { UseShellExecute = true });
+Console.WriteLine("See videos at "+curDirVideos);
+await context.CloseAsync();
+await browser.CloseAsync();
