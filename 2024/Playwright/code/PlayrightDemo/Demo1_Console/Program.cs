d6aef55b68f6793fdd559032fbb7f3a6c1be97db
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-12 22:11:03 +0300

    install

--- 2024/Playwright/code/PlayrightDemo/Demo1_Console/Program.cs
+++ 2024/Playwright/code/PlayrightDemo/Demo1_Console/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.Playwright;
-
+//https://playwright.dev/dotnet/docs/browsers
+var exitCode = Microsoft.Playwright.Program.Main(new[] { "install" });
+if (exitCode != 0)
+{
+    throw new Exception($"Playwright exited with code {exitCode}");
+}
 Console.WriteLine("Demo Playwright!");
 
 using var playwright = await Playwright.CreateAsync();
