64d31f75af65e94044681477f4a7e2a54ea47c48
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-11 16:35:34 +0200

    added Battle

--- compile/index.js
+++ compile/index.js
@@ -374,6 +374,18 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+
+
+folder='../2024/';
+file='Battle'
+fileHtml='presentation/Battle.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
+
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
     name: 'index.html',
