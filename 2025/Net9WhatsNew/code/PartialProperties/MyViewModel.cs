fd7e875aad32c054eb6a8b5b51350178ac7462e2
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-13 19:53:18 +0200

    added partial properties

---
+++ 2025/Net9WhatsNew/code/PartialProperties/MyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace test;
+
+
+public partial class MyViewModel :ObservableObject
+{
+    //see also https://ignatandrei.github.io/RSCG_Examples/v2/docs/CommunityToolkit.Mvvm#example--source-csproj-source-files-
+
+    [ObservableProperty]
+    public partial string? Name { get; internal set; }
+
+    //[RelayCommand]
+    //private void SayHello()
+    //{
+    //    Console.WriteLine("Hello");
+    //}
+}
