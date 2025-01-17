687bff94a784b32191bc2d91e60c3552fc9d63b9
Andrei Ignat <ignatandrei@yahoo.com>
2025-01-17 22:32:42 +0200

    2025

--- compile/index.js
+++ compile/index.js
@@ -385,6 +385,16 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+folder='../2025/';
+file='Net9WhatsNew'
+fileHtml='presentation/Net9WhatsNew.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
+
 
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
