4c697fec75f449f0220e1755ae28a7f28e1451dd
Andrei Ignat <ignatandrei@yahoo.com>
2025-03-19 21:20:40 +0200

    genai

--- compile/index.js
+++ compile/index.js
@@ -407,6 +407,18 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 archive.createZip();
 
 
+
+
+folder='../2025/';
+file='GenAIScript'
+fileHtml='presentation/GenAIScript.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
+
+
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
     name: 'index.html',
