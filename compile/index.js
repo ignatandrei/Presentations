a51b796f8fd1a7d5bbc65862346a3e03db078c50
Andrei Ignat <ignatandrei@yahoo.com>
2025-04-08 20:57:51 +0300

    Update index.js

--- compile/index.js
+++ compile/index.js
@@ -418,6 +418,14 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+folder='../2025/';
+file='Battle2025';
+fileHtml='presentation/Battle.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+ 
+// Create the zip file.
+archive.createZip();
 
 var folderToc = require("folder-toc");
 folderToc("../docs",  {
