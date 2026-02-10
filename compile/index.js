0d3e8362b5069d4cfcbc1f30ac5f64e971744bed
Andrei Ignat <ignatandrei@yahoo.com>
2026-02-10 15:10:44 +0200

    prez

--- compile/index.js
+++ compile/index.js
@@ -458,6 +458,14 @@ fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
 // Create the zip file.
 archive.createZip();
 
+folder='../2026/';
+file='PlaywrightMCPAgents';
+fileHtml='presentation/PlaywrightMCPAgents.html';  
+archive = new DirArchiver(`${folder}${file}`, `../docs/${file}.zip`,[]);
+fs.copyFileSync(`${folder}${file}/${fileHtml}`, `../docs/${file}prez.html`);
+
+// Create the zip file.
+archive.createZip();
 
 
 
